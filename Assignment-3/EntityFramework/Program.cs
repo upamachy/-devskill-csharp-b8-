fc2be2a2533c19f3457bd288042232bb101af6c8
Upama chowdhury <upamachy360@gmail.com>
2021-09-27 02:01:27 +0600

    Updated Assignment 3

--- Assignment-3/EntityFramework/Program.cs
+++ Assignment-3/EntityFramework/Program.cs
@@ -10,8 +10,11 @@ namespace EntityFramework
         static void Main(string[] args)
         {
             var context = new ManyToManyContext();
+            Console.WriteLine("Insert");
             Insert(context);
+            Console.WriteLine("Update");
             Update(context);
+            Console.WriteLine("Delete");
             Delete(context);
         }
 
@@ -38,16 +41,27 @@ namespace EntityFramework
             student1.Enrollments = new List<Enrollment> { new Enrollment { Course = course2 } };
             context.SaveChanges();
 
-          
+            var courses = context.Courses.Include(c => c.Enrollments).ThenInclude(e => e.Student).ToList();
 
+            foreach (var a in courses)
+            {
           
+                Console.WriteLine($"Id: {a.Id}, Course Title: {a.Title}, Fees: {a.Fees}, Duration Hour:  {a.DurationInHours}");
+                
+                foreach (var q in a.Enrollments)
+                {
+                    Console.WriteLine($"Id:{q.Student.Id}, Name: {q.Student.Name}, DateofBirth: {q.Student.DateOfBirth.Date}," +
+                        $" Address: {q.Student.Address}");
+                }
+            }
+
 
         }
         static void Update(ManyToManyContext context)
         {
            
             var student = context.Students.FirstOrDefault(s => s.Name == "Student1");
-            student.DateOfBirth = new DateTime(2011, 1, 1);
+            student.DateOfBirth = new DateTime(2005, 2, 5);
             context.Students.Update(student);
 
             var student3 = new Student { Name = "Murad", Address = "Sylhet", DateOfBirth = new DateTime(2012, 1, 1) };
@@ -61,6 +75,18 @@ namespace EntityFramework
             context.SaveChanges();
 
             
+            var courses = context.Courses.Include(c => c.Enrollments).ThenInclude(e => e.Student).ToList();
+            foreach (var item in courses)
+            {
+                Console.WriteLine($"Id: {item.Id}, Title: {item.Title}, Fees: {item.Fees}, Duration: {item.DurationInHours}");
+                foreach (var enroll in item.Enrollments)
+                {
+                    Console.WriteLine($"Id: {enroll.Student.Id}, Name: {enroll.Student.Name},  DateofBirth: {enroll.Student.DateOfBirth.Date}," +
+                        $" Address: {enroll.Student.Address}");
+                }
+            }
+
+
         }
         static void Delete(ManyToManyContext context)
         {
@@ -71,7 +97,16 @@ namespace EntityFramework
             var student = context.Students.FirstOrDefault(s => s.Name == "Murad");
             context.Students.Remove(student);
 
-            context.SaveChanges();   
+            context.SaveChanges();
+            var courses = context.Courses.Include(c => c.Enrollments).ThenInclude(e => e.Student).ToList();
+            foreach (var item in courses)
+            {
+                Console.WriteLine($"Id: {item.Id}, Name: {item.Title}, Fees: {item.Fees}, DurationHour: {item.DurationInHours}");
+                foreach (var enroll in item.Enrollments)
+                {
+                    Console.WriteLine($"Id: {enroll.Student.Id}, Name: {enroll.Student.Name},  DateofBirth: {enroll.Student.DateOfBirth.Date}, Address: {enroll.Student.Address}");
+                }
+            }
         }
     }
 }
